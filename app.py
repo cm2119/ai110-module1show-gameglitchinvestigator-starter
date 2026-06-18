7db4111239d4abeb50f8cf888f959f4a4b4489d3
cm2119 <cristina.isabelle.marcano@gmail.com>
2026-06-18 18:58:16 -0400

    fixed bug for new game and history/score/status

--- app.py
+++ app.py
@@ -125,6 +125,9 @@ with col3:
 if new_game:
     st.session_state.attempts = 0
     st.session_state.secret = random.randint(1, 100)
+    st.session_state.history = []
+    st.session_state.score = 0
+    st.session_state.status = "playing"
     st.success("New game started.")
     st.rerun()
 
